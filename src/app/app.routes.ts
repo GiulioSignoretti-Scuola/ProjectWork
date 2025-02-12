c5fafaa4b4e98e46df878384c61bf7c5b84edcf6
Francesco Agarri <francesco.agarri@gmail.com>
2025-02-12 13:26:36 +0100

    icona e componente prodotto, scheletro home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,10 +5,12 @@ import { HomeComponent } from './components/home/home.component';
 import { VassoioComponent } from './components/vassoio/vassoio.component';
 import { AboutuComponent } from './components/aboutu/aboutu.component';
 import { CarrelloComponent } from './components/carrello/carrello.component';
+import { ProdottoComponent } from './components/prodotto/prodotto.component';
 
 export const routes: Routes = [
     { path: '', component: WelcomeComponent},
     { path: 'home', component: HomeComponent },
+    { path: 'prodotto', component: ProdottoComponent},
     { path: 'vassoio', component: VassoioComponent},
     { path: 'about', component: AboutuComponent },
     { path: 'carrello', component: CarrelloComponent },
--- src/app/components/prodotto/prodotto.component.ts
+++ src/app/components/prodotto/prodotto.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ProdottiService } from '../../services/prodotti.service';
+import { prodotto } from '../../models/prodotto';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-prodotto',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './prodotto.component.html',
   styleUrl: './prodotto.component.css'
 })
 export class ProdottoComponent {
 
-}
+  prodotti: prodotto[] = [];
+
+  constructor(private prodottiServices: ProdottiService){
+    this.prodotti = prodottiServices.getProdottiByCategoria('panini');
+  }
+} 
