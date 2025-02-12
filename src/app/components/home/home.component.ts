94a41503b5b80488fc1b48a07dfaedee62d17ca1
Francesco Agarri <francesco.agarri@gmail.com>
2025-02-12 14:03:40 +0100

    settaggio home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import { SideBarComponent } from "../side-bar/side-bar.component";
+import { ProdottoComponent } from "../prodotto/prodotto.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, FooterComponent, SideBarComponent],
+  imports: [CommonModule, FooterComponent, SideBarComponent, ProdottoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
