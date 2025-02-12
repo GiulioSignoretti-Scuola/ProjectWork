38ca462d5d3c5478fc43c6683ddd55fc3ea71156
diegosucci07 <diegosucci2007@gmail.com>
2025-02-12 10:53:41 +0100

    sidebar+footer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
+import { SideBarComponent } from "../side-bar/side-bar.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [FooterComponent, SideBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
