037fee9337d4e34bb08ea01c7b9443b0ebbcd5c0
Francesco Agarri <francesco.agarri@gmail.com>
2025-02-13 16:25:23 +0100

    implementazione singolo prodotto

--- src/app/components/singolo-prodotto/singolo-prodotto.component.ts
+++ src/app/components/singolo-prodotto/singolo-prodotto.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 import { prodotto } from '../../models/prodotto';
 import { ProdottiService } from '../../services/prodotti.service';
 import { CommonModule } from '@angular/common';
+import { Ingrediente } from '../../models/ingredienti';
 
 @Component({
   selector: 'app-singolo-prodotto',
@@ -11,9 +12,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './singolo-prodotto.component.html',
   styleUrl: './singolo-prodotto.component.css'
 })
+
+
+
 export class SingoloProdottoComponent {
     prodotto: prodotto | undefined;
-  
+
     constructor(private prodottiServices: ProdottiService, private routes: ActivatedRoute){
   
       this.routes.paramMap.subscribe(params => {
@@ -21,5 +25,19 @@ export class SingoloProdottoComponent {
         if (Id != undefined)
           this.prodotto = prodottiServices.getProdottiById(Id);
       })
+    }
+
+    aggiungiIngrediente(i: Ingrediente){
+      if (i.Quantita <= i.QuantitaMin)
+        i.Quantita++;
+    }
+
+    diminuisciIngrediente(i: Ingrediente){
+      if (i.Quantita >= i.QuantitaMax)
+        i.Quantita--;
+    }
+
+    togliIngrediente(i: Ingrediente){
+      i.Quantita = 0;
     }
 }
